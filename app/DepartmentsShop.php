c6cfea622cc844c3b8fa986c34b74fae5a3157f2
Alex <Alex.Eremenko.Memento@gmail.com>
2021-04-12 06:44:55 +0300

    Append 'DepartmentShop' entity

---
+++ app/DepartmentsShop.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DepartmentsShop extends Model
+{
+    //
+}
---
+++ database/factories/DepartmentsShopFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\DepartmentsShop;
+use Faker\Generator as Faker;
+
+$factory->define(DepartmentsShop::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
